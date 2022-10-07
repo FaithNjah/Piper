b9d72d0b6891481f993c01dfaf687ba86ad22980
FaithNjah <faithnjah5@gmail.com>
2022-10-07 17:07:08 +0100

    v1 commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react"
+
+function App() {
+  
+
+  return (
+    <div className="App">
+    <h1 className="underline">hello world</h1>
+    </div>
+  )
+}
+
+export default App
